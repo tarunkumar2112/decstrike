2b657c02c0172a5bf20bbf375268da72b5d8a503
priyathakurstrix <priyathakur0527@gmail.com>
2022-12-24 10:23:07 +0530

    changes revert

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -6,22 +6,11 @@ import MyComponent from "../common/slide";
 const Footer = () => {
   return (
     <>
-      <div className="Marquee">
-  <div className="Marquee-content">
-    <div className="Marquee-tag">1</div>
-    <div className="Marquee-tag">2</div>
-    <div className="Marquee-tag">3</div>
-    <div className="Marquee-tag">1</div>
-    <div className="Marquee-tag">2</div>
-    <div className="Marquee-tag">3</div>
-    <div className="Marquee-tag">1</div>
-    <div className="Marquee-tag">2</div>
-    <div className="Marquee-tag">3</div>
-    <div className="Marquee-tag">1</div>
-    <div className="Marquee-tag">2</div>
-    <div className="Marquee-tag">3</div>
-  </div>
-</div>
+      <Container id='foot'className='p-3' fluid>
+       <Marquee>
+        <MyComponent />
+       </Marquee>
+      </Container>
     </>
   )
 };
