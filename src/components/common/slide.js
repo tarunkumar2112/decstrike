5cd776a051f5a8d6ce122dee2c7df346ac2305c0
priyathakurstrix <priyathakur0527@gmail.com>
2022-12-24 15:22:57 +0530

    test

--- src/components/common/slide.js
+++ src/components/common/slide.js
@@ -1,73 +1,105 @@
 import React from 'react'
-import Rashid from './rashid-shaheed-0c2cc8e.png'
+import Bjor from '../images/texas-tech-red-raiders.png'
+import Chase from '../images/chase.png'
+import Nick from '../images/nick-foles.png'
+import Arizona from '../images/arizona-state-sun-devils.png'
+import Duke from '../images/duke-blue-devils.png'
+import Hayden from '../images/hayden-hurst.png'
+import Westbrook from '../images/nick-westbrook-ikhine.png'
+import Sam from '../images/sam-ehlinger.png'
+import Zay from '../images/zay-jones.png'
+import Boise from '../images/boise-state-broncos.png'
+import Tech from '../images/texas-tech-red-raiders.png'
+import Clemson from '../images/clemson-tigers.png'
+import West from '../images/west-virginia-mountaineers.png'
 
 const slide = () => {
     return (
         <>
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Bjor} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">D. Smith
+</div><div className="top-mover-change-data green-int">$0.7103 (+1.41%)
+</div></div></div></div>
         </div>
+
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Chase} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">C. Edmonds
+</div><div className="top-mover-change-data red-int">$6.53 (0.76%)
+</div></div></div></div>
         </div>
+      
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Nick} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">N. Foles
+</div><div className="top-mover-change-data green-int">$28.16 (+0.91%)
+</div></div></div></div>
         </div>
+
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Arizona} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">E. Jones
+</div><div className="top-mover-change-data red-int">$1.33 (0.90%)
+</div></div></div></div>
         </div>
+
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Duke} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">R. Leonar
+</div><div className="top-mover-change-data green-int">$1.04 (+1.06%)
+</div></div></div></div>
         </div>
+
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Hayden} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">H. Hurst
+</div><div className="top-mover-change-data red-int">$4.73 (0.64%)
+</div></div></div></div>
         </div>
+
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Westbrook} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">N. Westbrook-Ikhine
+</div><div className="top-mover-change-data red-int">$2.86 (2.49%)
+</div></div></div></div>
         </div>
+
         <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
-        </div>
-        <div className='wrapper'>
-        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Rashid} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Huntley</div><div className="top-mover-change-data green-int">$6.20 (+3.15%)</div></div></div></div>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Sam} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">S. Ehlinger
+</div><div className="top-mover-change-data green-int">$1.28 (+2.02%)
+</div></div></div></div>
         </div>
         
+        <div className='wrapper'>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Zay} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">Z. Jones
+</div><div className="top-mover-change-data red-int">$8.25 (1.31%)
+</div></div></div></div>
+        </div>
+
+        <div className='wrapper'>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Boise} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Green
+</div><div className="top-mover-change-data green-int">$0.8503 (+1.17%)
+</div></div></div></div>
+        </div>
+
+        <div className='wrapper'>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Tech} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">T. Shough
+</div><div className="top-mover-change-data green-int">$1.41 (+0.80%)
+</div></div></div></div>
+        </div>
+
+        <div className='wrapper'>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Clemson} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">D. Uiagalelei
+</div><div className="top-mover-change-data green-int">$7.92 (+4.01%)
+</div></div></div></div>
+        </div>
+
+        <div className='wrapper'>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={Clemson} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">J. Daniels
+</div><div className="top-mover-change-data green-int">$1.32 (0.68%)
+</div></div></div></div>
+        </div>
+
+        <div className='wrapper'>
+        <div className="top-mover-card"><div className="ticker-athlete-image-div"><img src={West} alt="profile"></img></div><div className="top-mover-link"><div className="top-mover-athlete-info"><div className="top-mover-athlete-name">J. Daniels
+</div><div className="top-mover-change-data green-int">$1.32 (0.68%)
+</div></div></div></div>
+        </div>
+
         </>
     )
 }
